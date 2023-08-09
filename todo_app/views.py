5b14358af601027068b384852414ead1ec428791
siblun <118046824+siblun@users.noreply.github.com>
2023-08-09 14:46:31 +0700

    Add the ability to create items
    Took 1 minute

--- todo_app/views.py
+++ todo_app/views.py
@@ -11,3 +11,29 @@ class ListCreate(CreateView):
         context = super(ListCreate, self).get_context_data()
         context['title'] = 'Add a new list'
         return context
+
+
+class ItemCreate(CreateView):
+    model = ToDoItem
+    fields = [
+        'todo_list',
+        'title',
+        'description',
+        'due_date',
+    ]
+
+    def get_initial(self):
+        initial_data = super(ItemCreate, self).get_initial()
+        todo_list = ToDoList.objects.get(id=self.kwargs['list_id'])
+        initial_data['todo_list'] = todo_list
+        return initial_data
+
+    def get_context_data(self):
+        context = super(ItemCreate, self).get_context_data()
+        todo_list = ToDoList.objects.get(id=self.kwargs['list_id'])
+        context['todo_list'] = todo_list
+        context['title'] = 'Create a new item'
+        return context
+
+    def get_success_url(self):
+        return reverse('list', args=[self.object.todo_list_id])
