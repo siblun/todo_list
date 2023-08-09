7559fb5391dde147ce22156cdce130f11c0b24e2
siblun <118046824+siblun@users.noreply.github.com>
2023-08-09 14:45:25 +0700

    Add the ability to create lists
    Took 29 minutes

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView
+from .models import ToDoList, ToDoItem
 
-# Create your views here.
+
+class ListCreate(CreateView):
+    model = ToDoList
+    fields = ['title']
+
+    def get_context_data(self):
+        context = super(ListCreate, self).get_context_data()
+        context['title'] = 'Add a new list'
+        return context
