8a408e272c4d6d19cd0114d5f2a87e7a15f32216
siblun <118046824+siblun@users.noreply.github.com>
2023-08-19 10:46:22 +0700

    Add the documentation for apps.py
    Took 7 minutes

--- todo_app/apps.py
+++ todo_app/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class TodoAppConfig(AppConfig):
+    """
+        This class is used to configure the application.
+
+        Attributes
+        ----------
+        default_auto_field : str
+            the implicit primary key type to add to models within this app
+        name : str
+            defines which application the configuration applies to
+        """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo_app'
